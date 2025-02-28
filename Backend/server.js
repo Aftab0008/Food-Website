5a02c652488c2e86c3c03e78cecc77f1e405c625
Aftab0008 <aftabalam4331@gmail.com>
2025-03-01 04:36:44 +0530

    Fixed esbuild permissions

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./authRoutes.js";
-import reviewRoutes from "./reviewRoutes.js";
-import reservationRoutes from "./reservationRoutes.js"; // Import reservation routes
 
 dotenv.config();
 
@@ -13,15 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
 
-// Health Check Route (Prevents Railway from stopping the app)
-app.get("/", (req, res) => {
-  res.send("Server is running on Railway!");
-});
-
 // API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/reviews", reviewRoutes);
-app.use("/api/reservations", reservationRoutes);
+app.use("/api", authRoutes);
+
 
 // Start the server
 const PORT = process.env.PORT || 5000;
