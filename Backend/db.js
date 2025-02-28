f61828885abc88ed6b17662432168a05525ebb47
Aftab0008 <aftabalam4331@gmail.com>
2025-03-01 04:44:48 +0530

    Updated .env and db.js for Railway deployment

--- Backend/db.js
+++ Backend/db.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT), // Convert port to a number
+  
 });
 
 pool.connect()
